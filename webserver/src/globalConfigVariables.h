3b7f9e809a2746e87c464be8c217c5d956c9eab9
K1N62 <jiah13@student.bth.se>
2015-09-24 15:44:25 +0200

    Added header for global config variables

---
+++ webserver/src/globalConfigVariables.h
@@ -0,0 +1,12 @@
+
+/* Global Config Variables Header file
+ *
+ * Contains all the servers configuration Variables
+ * These are set to the default values and later overwritten by the config
+ *  or parameter settings
+ *
+ */
+
+#define LISTEN_IP     127.0.0.1
+#define LISTEN_PORT   80
+#define ROOT_DIR      "../../www"
