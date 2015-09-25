ef822302b45cfea362eace3f4f5aa42978f4aea6
K1N62 <jiah13@student.bth.se>
2015-09-25 14:15:09 +0200

    Changed name of logs to extension .log

--- webserver/src/globalConfigVariables.h
+++ webserver/src/globalConfigVariables.h
@@ -12,10 +12,6 @@
 
 char *SERVERNAME      = "127.0.0.1";
 int   LISTEN_PORT     = 80;
-<<<<<<< HEAD
 char *BASE_DIR        = "../../www";
-=======
-char *ROOT_DIR        = "../../www";
-char *SERVER_LOG_PATH = "../log/serverlog";
-char *ACCESS_LOG_PATH = "../log/accesslog";
->>>>>>> c134924774c2c0876c63e5ec591057a7f261bece
+char *SERVER_LOG_PATH = "../log/server.log";
+char *ACCESS_LOG_PATH = "../log/access.log";
