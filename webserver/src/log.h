c1c7146d336d5646e334990008678d5b802246a9
K1N62 <jiah13@student.bth.se>
2015-09-27 00:56:46 +0200

    Added log levels and included httpd.h

--- webserver/src/log.h
+++ webserver/src/log.h
@@ -3,7 +3,15 @@
  * Includes headers important for logging functions
  */
 
- #pragma once
+#pragma once
+
+#include "httpd.h"
+
+#define LOG_CRITICAL  0
+#define LOG_ERROR     1
+#define LOG_WARN      2
+#define LOG_INFO      3
+#define LOG_DEBUG     4
 
 void accesslog(const struct sockaddr *addr, char* request, int code, int byte);
 void serverlog(int error, char* errorMessage);
