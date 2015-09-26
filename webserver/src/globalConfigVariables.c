dabf2f33de66ff2ee133d5df9fd14ecc33b54faf
K1N62 <jiah13@student.bth.se>
2015-09-27 00:56:15 +0200

    Still obsolete, dosn't work

---
+++ webserver/src/globalConfigVariables.c
@@ -0,0 +1,8 @@
+
+#include "globalConfigVariables.h"
+
+char *SERVERNAME      = "127.0.0.1";
+int   LISTEN_PORT     = 80;
+char *BASE_DIR        = "../../www";
+char *SERVER_LOG_PATH = "../log/server.log";
+char *ACCESS_LOG_PATH = "../log/access.log";
