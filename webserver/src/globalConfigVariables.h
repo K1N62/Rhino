605c79b450a581fb731963badcdb8b398716a3d3
K1N62 <jiah13@student.bth.se>
2015-09-27 00:55:54 +0200

    Changed to declarations only

--- webserver/src/globalConfigVariables.h
+++ webserver/src/globalConfigVariables.h
@@ -9,8 +9,8 @@
 
 #pragma once
 
-char *SERVERNAME      = "127.0.0.1";
-int   LISTEN_PORT     = 80;
-char *BASE_DIR        = "../../www";
-char *SERVER_LOG_PATH = "../log/server.log";
-char *ACCESS_LOG_PATH = "../log/access.log";
+char *SERVERNAME;
+int   LISTEN_PORT;
+char *BASE_DIR;
+char *SERVER_LOG_PATH;
+char *ACCESS_LOG_PATH;
