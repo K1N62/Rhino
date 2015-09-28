68280c39a38dbc0fb946d36f9b8b84735a95b6d4
lula13 <lula13@student.bth.se>
2015-09-28 09:34:36 +0200

    Coding log

--- webserver/src/log.c
+++ webserver/src/log.c
@@ -3,12 +3,23 @@
 
 void accesslog(const struct sockaddr *addr, char* request, int code, int byte)
 {
+  int fd;
+  char* accLogPath = logPath + "/accessLog";
+
+  fd = open(accLogPath, O_CREAT | O_RDWR);
+
+  char* logMessage = addr.ai_addr + " - - " + date()
 
 }
 
 void serverlog(int error, char* errorMessage)
 {
   int fd;
+  char* serLogPath = logPath + "/serverLog";
 
-  fd = open(SERVER_LOG_PATH, O_CREAT | O_RDWR);
+  fd = open(serLogPath, O_CREAT | O_RDWR);
+
+  char* message = date() + ' ' + (error + '0') ' ' + errorMessage;
+  write(fd, message, sizeof(message));
+  close (fd);
 }
